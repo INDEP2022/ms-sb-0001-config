deeb93eed83dc5bfd8e665ae70b7f049b7d576f2
Fernando Aponte <fernandoaponte90@gmail.com>
2023-03-29 20:52:11 -0400

    fix: set ApiResponse as type string

--- src/modules/application/application.controller.ts
+++ src/modules/application/application.controller.ts
@@ -5,20 +5,19 @@ import { ApiBody, ApiOperation, ApiQuery, ApiResponse, ApiTags } from "@nestjs/s
 import { Paginate, PaginateQuery } from 'nestjs-paginate';
 import { Body, Post, Query } from "@nestjs/common/decorators";
 import { ApplicationService } from "./application.service";
-// import { QueryParams, ResponseDataDTO } from "sigebi-lib-common";
 
 @ApiTags('config applications ')
 @Controller('config/apps')
 export class ApplicationController {
     constructor(private readonly service: ApplicationService) { }
 
-    @ApiOperation({ summary: 'Procedure insertMotivosRev' })
-    // @ApiResponse({
-    //     status: 200,
-    //     type: ResponseDataDTO
-    // })
+    @ApiOperation({ summary: 'Procedure SP_INSERTA_MOTIVOSREV' })
+    @ApiResponse({
+        status: 200,
+        type: String,
+    })
     @Post("/insertMotivosRev")
-    async registerGoodTrackerNsbdb1(@Body() goodInNumber: number, eventInId: number, reasonsIn: string, reasonsInNumber: string, actionIn: string) {
+    async insertMotivosRev(@Body() goodInNumber: number, eventInId: number, reasonsIn: string, reasonsInNumber: string, actionIn: string) {
         return this.service.insertMotivosRev(goodInNumber, eventInId, reasonsIn, reasonsInNumber, actionIn);
     }
 }
