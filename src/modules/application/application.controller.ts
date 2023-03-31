ba14b9b0eb8edba8e3f15691f310cf2047c12e1f
Fernando Aponte <fernandoaponte90@gmail.com>
2023-03-30 23:31:10 -0400

    feat: added correct entities to use

--- src/modules/application/application.controller.ts
+++ src/modules/application/application.controller.ts
@@ -6,6 +6,7 @@ import { Paginate, PaginateQuery } from 'nestjs-paginate';
 import { Body, Post, Query } from "@nestjs/common/decorators";
 import { ApplicationService } from "./application.service";
 import { ResponseDataDTO } from "src/shared/dto/response.data.dto";
+import { InsertReasonsRevDto } from "./dto/param.dto";
 
 @ApiTags('config applications ')
 @Controller('config/apps')
@@ -13,12 +14,13 @@ export class ApplicationController {
     constructor(private readonly service: ApplicationService) { }
 
     @ApiOperation({ summary: 'Procedure SP_INSERTA_MOTIVOSREV' })
+    @ApiBody({ type: InsertReasonsRevDto })
     @ApiResponse({
         status: 200,
         type: ResponseDataDTO,
     })
-    @Post("/insertMotivosRev")
-    async insertMotivosRev(@Body() goodInNumber: number, eventInId: number, reasonsIn: string, reasonsInNumber: string, actionIn: string) {
-        return this.service.insertMotivosRev(goodInNumber, eventInId, reasonsIn, reasonsInNumber, actionIn);
+    @Post("/insertReasonsRev")
+    async insertReasonsRev(@Body() dto: InsertReasonsRevDto) {
+        return this.service.insertReasonsRev(dto);
     }
 }
