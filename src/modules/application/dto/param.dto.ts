d95c060d393a321064c98c50d531f5d3866a3cb8
Fernando Aponte <fernandoaponte90@gmail.com>
2023-03-31 20:08:51 -0400

    feat: added util and message

--- src/modules/application/dto/param.dto.ts
+++ src/modules/application/dto/param.dto.ts
@@ -1,27 +1,28 @@
 import { Type } from "class-transformer";
 import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber, IsString, Length } from "class-validator";
+import { Message } from "src/shared/utils/message.decorator";
 
 export class InsertReasonsRevDto {
 
     @Type(() => Number)
-    @IsNumber({}, { message: "debe ser numerico" })
+    @IsNumber({}, { message: Message.NUMBER('$property') })
     goodInNumber: number;
 
     @Type(() => Number)
-    @IsNumber({}, { message: "debe ser numerico" })
+    @IsNumber({}, { message: Message.NUMBER('$property') })
     eventInId: number;
 
     @Type(() => String)
-    @IsString({ message: "debe ser cadena de texto" })
+  @IsString({ message: Message.STRING('$property') })
     reasonsIn: string;
 
     @Type(() => Number)
-    @IsNumber({}, { message: "debe ser numerico" })
+    @IsNumber({}, { message: Message.NUMBER('$property') })
     reasonsInNumber: string;
 
     @Type(() => String)
-    @IsString({ message: "debe ser cadena de texto" })
+  @IsString({ message: Message.STRING('$property') })
     actionIn: string;
 
 }
